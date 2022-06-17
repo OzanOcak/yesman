346171233202740eefe24c89e53db784d6a772dd
OzanOcak <ozanocak@gmail.com>
2022-06-16 22:38:52 -0400

    setup

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+import Head from "next/head";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Heading>Ozan coding</Heading>
+    </div>
+  );
+};
+
+export default Home;
