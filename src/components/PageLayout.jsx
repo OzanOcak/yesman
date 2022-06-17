f31ba052a943f4ec2a46572200004e689d120dfd
OzanOcak <ozanocak@gmail.com>
2022-06-17 11:36:52 -0400

    homepage

---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Container, Center, Text } from "@chakra-ui/react";
+
+import AppHeader from "components/AppHeader";
+//import ToggleColorModeButton from "components/ToggleColorModeButton";   <ToggleColorModeButton />;
+
+const PageLayout = ({ children }) => {
+  return (
+    <>
+      <AppHeader />
+
+      <Container px={4} maxW="5xl">
+        {children}
+      </Container>
+
+      <Center m="2rem 0">
+        <Text>© Copyright Cules Coding 2022</Text>
+      </Center>
+    </>
+  );
+};
+
+export default PageLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
-import { Heading } from "@chakra-ui/react";
+import BlogPreviewList from "components/blog/BlogPreviewList";
 import Head from "next/head";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <Heading>Ozan coding</Heading>
-    </div>
+    <>
+      <Head>
+        <title>makeItYes</title>
+      </Head>
+      <title>Ozan coding</title>
+
+      <BlogPreviewList />
+    </>
   );
 };
 
